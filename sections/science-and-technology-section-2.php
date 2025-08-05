7e1dfb8ccc849b873b138aeca3191a101df08f07
Sardar Ali <sardaralikhamosh@gmail.com>
2025-08-06 00:16:10 +0500

    added
    new images and content

--- sections/science-and-technology-section-2.php
+++ sections/science-and-technology-section-2.php
@@ -14,7 +14,7 @@
                 <!-- Image Column with Angle -->
                 <div class="col-lg-7 col-md-12 about-image-container">
                     <div class="about-image displaynon-onmobile">
-                        <img src="./images/section-3-updated.png" alt="Elemental Biologics Laboratory" class="img-fluid">
+                        <img src="./images/new-image-0.png" alt="Elemental Biologics Laboratory" class="img-fluid">
                     </div>
                 </div>
                 
@@ -31,7 +31,7 @@
                 <!-- below section is only for mobile  -->
                  <div class="col-lg-7 col-md-12 about-image-container">
                     <div class="about-image displaynon-onlaptop">
-                        <img src="./images/section-3-updated.png" alt="Elemental Biologics Laboratory" class="img-fluid">
+                        <img src="./images/new-image-0.png" alt="Elemental Biologics Laboratory" class="img-fluid">
                     </div>
                 </div>
             </div>
--- sections/science-and-technology-section-5.php
+++ sections/science-and-technology-section-5.php
@@ -14,7 +14,7 @@
                 <!-- Image Column with Angle -->
                 <div class="col-lg-7 col-md-12 about-image-container">
                     <div class="about-image displaynon-onmobile">
-                        <img src="./images/sections/science-and-technology-section-4.jpg" alt="Elemental Biologics Laboratory" class="img-fluid ">
+                        <img src="./images/new-image-2.png" alt="Elemental Biologics Laboratory" class="img-fluid ">
                     </div>
                 </div>
                 
@@ -32,7 +32,7 @@
                  <div class="col-lg-7 col-md-12 about-image-container">
                     <div class="about-image <div class="col-lg-7 col-md-12 about-image-container">
                     <div class="about-image displaynon-laptop">
-                        <img src="./images/sections/science-and-technology-section-4.jpg" alt="Elemental Biologics Laboratory" class="img-fluid ">
+                        <img src="./images/new-image-2.png" alt="Elemental Biologics Laboratory" class="img-fluid ">
                     </div>
                 </div>
             </div>
